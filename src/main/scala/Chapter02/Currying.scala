c2f416e199028d9df77c0c946490ad90623830f3
Dirk Becker-Horst <dirk@becker-horst.com>
2017-10-29 21:01:50 +0100

    Added currying

---
+++ src/main/scala/Chapter02/Currying.scala
@@ -0,0 +1,17 @@
+package Chapter02
+
+class Currying {
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (B => f(a, B))
+  }
+
+  def uncurry[A, B, C](f: A => (B => C)): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a: A => f(g(a))
+  }
+
+}
